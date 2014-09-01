be6fe6dc0b4d01ba0b128b9c73488b238f5e497a
taoyuan <torworx@gmail.com>
2014-09-01 20:58:04 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,94 @@
+cancellable
+===========
+
+[![NPM Version](https://img.shields.io/npm/v/cancellable.svg?style=flat)](https://www.npmjs.org/package/cancellable)
+[![Build Status](http://img.shields.io/travis/taoyuan/cancellable.svg?style=flat)](https://travis-ci.org/taoyuan/cancellable)
+[![Dependencies](https://img.shields.io/david/taoyuan/cancellable.svg?style=flat)](https://david-dm.org/taoyuan/cancellable)
+
+> A library for making async operations cancellable
+
+## Installation
+
+```bash
+$ npm install cancellable --save
+```
+
+## API
+
+### cancellable([fn])
+
+Returns a new cancellable:
+
+```js
+var cancellable = require('cancellable');
+
+var fn = cancellable();
+callAsyncOperation(arg1, arg2, arg3, fn.future);
+
+setTimeout(function () {
+    fn.cancel('Operation timed out');
+}, 1000);
+```
+
+### cancellable.empty()
+
+Returns an 'empty' cancellable future (one that will never be cancelled).
+
+```js
+var cancellable = require('cancellable');
+function asyncOperation(arg1, arg2, arg3, callback) {
+    callback = callback || cancellable.empty();
+
+    // Continue with function knowing there is a cancellable future
+}
+```
+
+### future.cancelled([callback])
+
+If no arguments provide, returns true if the future has been cancelled:
+
+```js
+//In ES6
+function asyncOperation(future) {
+  return spawn(function* () {
+    while(!future.cancelled()) {
+      yield NextAsyncOp();
+    }
+  })
+}
+```
+
+If cancelled with a callback, calls callback when the future is cancelled 
+(this is probably currently the most useful of these methods).
+
+```javascript
+function get(url, future) {
+    var req = request(url, future);
+
+    future.cancelled(function (reason) {
+        future(reason);
+        req.abort();
+    });
+}
+```
+
+### future.throwIfCancelled()
+
+Throws the reason if the future has been cancelled:
+
+```javascript
+//In ES6
+function asyncOperation(future) {
+    return spawn(function* () {
+        while(true) {
+            future.throwIfCancelled()
+            yield NextAsyncOp();
+        }
+    })
+}
+```
+
+## License
+
+Copyright (c) 2014 Tao Yuan  
+Licensed under the MIT license.
---
+++ test/cancellable.test.js
@@ -0,0 +1,170 @@
+"use strict";
+
+var async = require('async');
+var t = require('chai').assert;
+var cancellable = require('../lib/cancellable');
+
+describe.only('cancellable', function () {
+
+    describe('Default token fn', function () {
+        it('should not get cancelled', function (done) {
+            delay(10, done);
+        });
+    });
+
+    describe('Cancelling with a future', function () {
+        it('should invoke callback with reason in the next turn of the event loop', function (done) {
+            var fn = cancellable();
+            var waitedTillNextTurn = false;
+            var timeout;
+            fn.cancel('Test Cancel');
+            waitedTillNextTurn = true;
+            timeout = setTimeout(function () {
+                throw new Error('Did not cancel fast enough');
+            }, 100);
+
+            delay(20000, fn.future, function (reason) {
+                if (!reason) throw new Error('Should have been cancelled');
+                t(waitedTillNextTurn);
+                t.instanceOf(reason, Error);
+                t.equal(reason.message, 'Test Cancel');
+                t.equal(reason.code, 'OperationCancelled');
+                clearTimeout(timeout);
+                done();
+            });
+        });
+    });
+
+    describe('Polling for cancellable', function () {
+        describe('using `.cancelled()`', function () {
+            it('should works', function (done) {
+                var fn = cancellable();
+                var timeout;
+                fn.cancel('Test Cancel');
+                timeout = setTimeout(function () {
+                    throw new Error('Did not cancel fast enough');
+                }, 30);
+                delay2(40, fn.future, function (reason) {
+                    if (!reason) throw new Error('Should have been cancelled');
+                    clearTimeout(timeout);
+                    done();
+                });
+            });
+        });
+        describe('using `.throwIfCancelled()`', function () {
+            it('should works', function (done) {
+                var fn = cancellable();
+                var timeout;
+                fn.cancel('Test Cancel');
+                timeout = setTimeout(function () {
+                    throw new Error('Did not cancel fast enough');
+                }, 30);
+                delay3(40, fn.future, function (reason) {
+                    if (!reason) throw new Error('Should have been cancelled');
+                    clearTimeout(timeout);
+                    done();
+                });
+            });
+        });
+    });
+
+    describe('Cascading cancellable', function () {
+        it('should works', function (done) {
+            var fn = cancellable();
+            var waitedTillNextTurn = false;
+            var timeout;
+            fn.cancel('Test Cancel');
+            waitedTillNextTurn = true;
+            timeout = setTimeout(function () {
+                throw new Error('Did not cancel fast enough');
+            }, 20);
+            cascade(fn.future, function (reason) {
+                if (!reason) throw new Error('Should have been cancelled');
+                t(waitedTillNextTurn);
+                t.instanceOf(reason, Error);
+                t.equal(reason.message, 'Test Cancel');
+                t.equal(reason.code, 'OperationCancelled');
+                clearTimeout(timeout);
+                done();
+            });
+        });
+    });
+});
+
+
+function delay(timeout, future, cb) {
+    if (typeof future === 'function' && !future.cancelled) {
+        cb = future;
+        future = null;
+    }
+    future = future || cancellable.empty();
+
+    var called = false;
+
+    function done(err) {
+        if (called) return;
+        called = true;
+        cb(err);
+    }
+
+    setTimeout(done, timeout);
+    future.cancelled(done);
+}
+
+function delay2(timeout, future, cb) {
+    if (typeof future === 'function' && !future.cancelled) {
+        cb = future;
+        future = null;
+    }
+    future = future || cancellable.empty();
+    setTimeout(function () {
+        if (future.cancelled()) return cb(new Error('Operation Cancelled'));
+        cb();
+    }, timeout / 4);
+}
+
+function delay3(timeout, future, cb) {
+    if (typeof future === 'function' && !future.cancelled) {
+        cb = future;
+        future = null;
+    }
+    future = future || cancellable.empty();
+
+    async.series([
+        function (callback) {
+            delay(timeout / 4, callback);
+        },
+        function (callback) {
+            safeThrowIfCancelled(callback);
+        },
+        function (callback) {
+            safeThrowIfCancelled(callback);
+        },
+        function (callback) {
+            safeThrowIfCancelled(callback);
+        }
+    ], cb);
+
+    function safeThrowIfCancelled(callback) {
+        try {
+            future.throwIfCancelled();
+            delay(timeout / 4, callback);
+        } catch (e) {
+            callback(e);
+        }
+    }
+}
+
+function cascade(future, cb) {
+    async.series([
+        function (callback) {
+            delay(500, future, callback);
+        },
+        function (callback) {
+            delay(500, future, callback);
+        },
+        function (callback) {
+            delay(500, future, callback);
+        }
+    ], cb);
+}
